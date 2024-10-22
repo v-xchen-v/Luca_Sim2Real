e07a75d70783a66f736846b533fd5947df3720c6
Xi Chen <407081305@qq.com>
2024-10-22 19:41:30 +0800

    update setup.py that adds point cloud libraries

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='my_package',
+    name='luca_real2sim',
     version='0.1.0',
     packages=['luca_transformation'],
     install_requires=[
         'pytransform3d',
         'opencv-python',
         'pyrealsense2',
-        'torch', # only for load sim trajectory data, not necessary for the main functionality
+        # 'torch', # only for load sim trajectory data, not necessary for the main functionality,
+        "open3d",
+        'pyglet==1.5.27'
     ],
     python_requires='>=3.8.10',
 )
