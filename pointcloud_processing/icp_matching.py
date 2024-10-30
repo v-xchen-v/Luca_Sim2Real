95ee56f83cc1ee834bdcb9b001069d56549b120c
Xi Chen <407081305@qq.com>
2024-10-30 17:27:49 +0800

    optimize icp and tested on coke and orange

--- pointcloud_processing/icp_matching.py
+++ pointcloud_processing/icp_matching.py
@@ -29,6 +29,16 @@ def icp_align(source, target, threshold=0.01, max_iterations=50):
     
     target_filtered, _ = target.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
     
+    # # Compute a mesh from the source point cloud
+    # source_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(source, alpha=0.05)
+    # source_mesh.compute_vertex_normals()
+
+    # # Uniformly sample points from the mesh to upsample the source point cloud
+    # upsampled_source = source_mesh.sample_points_uniformly(number_of_points=10000)
+
+    # print(f"Original points: {len(source.points)}, Upsampled points: {len(upsampled_source.points)}")
+
+
     # debugging visualization for filtered target
     if False:
         o3d.visualization.draw_geometries([source, target_filtered])
@@ -61,12 +71,15 @@ def icp_align(source, target, threshold=0.01, max_iterations=50):
     source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
     target_filtered.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
-
+    # Seems point_to_point is better than point_to_plane
     print("Coarse ICP...")
+    threshold = 0.1  # Larger threshold for coarse alignment
     reg_p2p_coarse = o3d.pipelines.registration.registration_icp(
         source, target_filtered, threshold, init_transformation,
         o3d.pipelines.registration.TransformationEstimationPointToPoint(),
-        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=50))
+        # o3d.pipelines.registration.TransformationEstimationPointToPlane(),
+        o3d.pipelines.registration.ICPConvergenceCriteria(
+            max_iteration=2000,relative_fitness=1e-4, relative_rmse=1e-4))
 
     # Use the result as the initial guess for fine alignment
     print("Fine ICP...")
@@ -75,7 +88,9 @@ def icp_align(source, target, threshold=0.01, max_iterations=50):
     reg_p2p = o3d.pipelines.registration.registration_icp(
         source, target_filtered, threshold_fine, reg_p2p_coarse.transformation,
         o3d.pipelines.registration.TransformationEstimationPointToPoint(),
-        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=50))
+        # o3d.pipelines.registration.TransformationEstimationPointToPlane(),
+        o3d.pipelines.registration.ICPConvergenceCriteria(
+            max_iteration=2000,relative_fitness=1e-6, relative_rmse=1e-6))
 
     # # Apply ICP
     # reg_p2p = o3d.pipelines.registration.registration_icp(
@@ -116,7 +131,7 @@ def align_source_to_target(source, target):
 
     # debugging visualization for visual centered source and target
     # visual centered source and target
-    if False:
+    if True:
         o3d.visualization.draw_geometries([source_centered, target_centered])
     
     # Restore both point clouds to their original positions
