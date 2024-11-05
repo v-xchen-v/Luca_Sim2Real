22ab8a63209eeffb00f6f933444f055478149e5c
v_xchen_v <407081305@qq.com>
2024-11-05 21:45:35 +0800

    notes

--- catkin_ws/src/grasp_demo_app/README.md
+++ catkin_ws/src/grasp_demo_app/README.md
@@ -1,5 +1,17 @@
 # Uni Grasp Demo App
 ## TODOs
-- 
+- Simplest dummy pipe gohome-> grasp -> gohome
+- + place object to blanket
+- Traj generator
+- Issues to fix
+- Workspace setup
+- Test
+- More traj of objects
+- ICP to match
 
 ## How to launch ros service to control robot right arm and hand?
+
+
+## Changelog
+- execution ready for gohome-> grasp(traj) -> gohome 11.05
+- 
--- catkin_ws/src/grasp_demo_app/scripts/main.py
+++ catkin_ws/src/grasp_demo_app/scripts/main.py
@@ -1,9 +1,29 @@
+from core.grasp_and_place_executor import GraspAndPlaceExecutor
+
 def setup_scene():
     pass
 
+def generate_executable_trajectory():
+    pass
+
+def execuate_trajectory():
+    pass
 
 def main():
+    
+    grasp_and_place_executor = GraspAndPlaceExecutor()
+    
+    repeat_times = 10
     setup_scene()
-    for i in range(10):
-        grasp_and_place = GraspAndPlace('trace.npy')
-        grasp_and_place.run()
+    
+    for i in range(repeat_times):
+        
+        grasp_and_place_executor.goto_home()
+        
+        generate_executable_trajectory()
+        
+        grasp_and_place_executor.grasp('trace.npy')
+        
+        grasp_and_place_executor.goto_home()
+        
+        # press key to continue, for dummy
