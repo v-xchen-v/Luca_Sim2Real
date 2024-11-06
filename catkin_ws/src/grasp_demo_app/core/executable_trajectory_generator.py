b6dd96dada8c6bbaff9f8fd24ebd9a3b840a1bf0
Xi Chen <407081305@qq.com>
2024-11-06 11:30:55 +0800

    first design of ExecutableTrajectoryGenerator

---
+++ catkin_ws/src/grasp_demo_app/core/executable_trajectory_generator.py
@@ -0,0 +1,24 @@
+"""Capture Scene, Determine and Locate Object, Generate Trajectory"""
+
+
+from trajectory_processing.trajectory_adaptor import TrajectoryAdaptor
+
+class ExecutableTrajectoryGenerator:
+    def __init__(self) -> None:
+        pass
+    
+    
+    def capture_scene(self):
+        pass
+    
+    
+    def determine_object(self):
+        pass
+    
+    
+    def locate_object(self):
+        pass
+    
+    
+    def generate_trajectory(self):
+        pass
