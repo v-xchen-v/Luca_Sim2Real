e2bc11d7d2adfc16ceea80fab402eaf9f7428a1b
Xi Chen <407081305@qq.com>
2024-11-08 07:54:04 +0800

    dummy test script

--- catkin_ws/src/grasp_demo_app/debug/sim2_traj_processor/icp_sim2real_tral_with_restrict_table_zone.py
+++ catkin_ws/src/grasp_demo_app/debug/sim2_traj_processor/icp_sim2real_tral_with_restrict_table_zone.py
@@ -6,14 +6,10 @@ if module_path not in sys.path:
 from core.sim2real_trajectory_processor import Sim2RealTrajectoryProcessor
 
 # Example usage
-processor = Sim2RealTrajectoryProcessor()
+processor = Sim2RealTrajectoryProcessor(config='catkin_ws/src/grasp_demo_app/config/debug_cam_calibration_config_11f_coke.json')
 table_width = 0.6
 table_height = 0.6
 table_thickness = 0.3
 processor.setup_robot_table(table_dimensions=[table_width, table_height, table_thickness])
-processor.configure_object_settings(object_idx=3)  # Example: Cube configuration
-processor.load_sim_trajectory()
-processor.locate_object(x_keep_range=[-0.30, 0], y_keep_range=[-0.05, 0.15], z_keep_range=[None, -0.011])
-processor.map_sim_to_real()
-processor.compute_real_hand_to_robot_base_transform()
-processor.save_real_trajectory()
+print("Robot table setup completed.")
+print(processor.restricted_table_no_touch_zone)
