2ad0be7b8c899c34b3a202a77925becdc08c4ffa
Xi Chen <407081305@qq.com>
2024-11-12 17:38:57 +0800

    sh for run demo app

--- run/grasp_demo_app.sh
+++ run/grasp_demo_app.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+# chmod +x run/grasp_demo_app.sh
+
 # Define workspace variables
 WORK="/work"
 WORK1="/work1"
+# WORK="~/opt/ros/noetic"
+# WORK1="/home/yichao/Documents/repos/Luca_Transformation"
+
 
 # Set up for ROS1
 echo "Sourcing ROS1 setup..."
 source "$WORK/devel/setup.bash"
 
 # Set up for ROS package
-echo "Building ROS package..."
-cd "$WORK1/catkin_ws"
-catkin_make
-source "$WORK1/catkin_ws/devel/setup.bash"
-
-# Optional: Remove previous builds
+## Optional: Remove previous builds
 read -p "Do you want to remove previous builds? (y/n): " yn
 case $yn in
     [Yy]* ) 
@@ -24,19 +24,29 @@ case $yn in
     * ) echo "Skipping removal of previous builds.";;
 esac
 
-# Launch ROS node in new terminal
-echo "Launching ROS node..."
-gnome-terminal -- bash -c "roslaunch grasp_demo_app debug_moveit.launch; exec bash"
+echo "Building ROS package..."
+cd "$WORK1/catkin_ws"
+catkin_make
+source "$WORK1/catkin_ws/devel/setup.bash"
+
 
 # Optional: Prepare before launching app (load model and check camera)
 read -p "Do you want to run pre-launch checks? (y/n): " yn
 case $yn in
     [Yy]* ) 
         echo "Running pre-launch checks..."
-        gnome-terminal -- bash -c "cd $WORK1; python catkin_ws/src/scripts/checking.py; exec bash";;
+        cd "$WORK1"
+        python scripts/checking.py
+        echo "Pre-launch checks completed.";;
     * ) echo "Skipping pre-launch checks.";;
 esac
 
 # Launch app in new terminal
 echo "Launching application..."
-gnome-terminal -- bash -c "cd $WORK1; python catkin_ws/src/grasp_demo_app/scripts/main.py; exec bash"
+cd "$WORK1"
+python catkin_ws/src/grasp_demo_app/scripts/main.py
+echo "Application has finished."
+
+
+# Run in new terminal
+# roslaunch grasp_demo_app debug_moveit.launch
