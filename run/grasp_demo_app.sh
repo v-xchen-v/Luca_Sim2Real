ff375cc7b4b897d07be52a7e49064672c3bbee49
See <407081305@qq.com>
2024-11-12 18:45:44 +0900

    Prepare sh script for running app in docker (#53)
    * command tips
    
    * a command of roslaunch robot control service
    
    * script for run demo app
    
    * sh for run demo app
    
    * sh for run grasp app in docker

---
+++ run/grasp_demo_app.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# chmod +x run/grasp_demo_app.sh
+
+# Define workspace variables
+WORK="/work"
+WORK1="/work1"
+# WORK="~/opt/ros/noetic"
+# WORK1="/home/yichao/Documents/repos/Luca_Transformation"
+
+# Optional: Kill all Python processes, to avoid camera issues
+read -p "Do you want to forcefully kill all Python processes? (y/n): " yn
+case $yn in
+    [Yy]* ) 
+        echo "Killing all Python processes..."
+        pkill -9 -f python
+        echo "All Python processes killed.";;
+    * ) echo "Skipping killing Python processes.";;
+esac
+
+# Set up for ROS1
+echo "Sourcing ROS1 setup..."
+source "$WORK/devel/setup.bash"
+
+# Set up for ROS package
+## Optional: Remove previous builds
+read -p "Do you want to remove previous builds? (y/n): " yn
+case $yn in
+    [Yy]* ) 
+        echo "Removing previous builds..."
+        rm -rf "$WORK1/catkin_ws/devel"
+        rm -rf "$WORK1/catkin_ws/build";;
+    * ) echo "Skipping removal of previous builds.";;
+esac
+
+echo "Building ROS package..."
+cd "$WORK1/catkin_ws"
+catkin_make
+source "$WORK1/catkin_ws/devel/setup.bash"
+
+
+# Optional: Prepare before launching app (load model and check camera)
+read -p "Do you want to run pre-launch checks? (y/n): " yn
+case $yn in
+    [Yy]* ) 
+        echo "Running pre-launch checks..."
+        cd "$WORK1"
+        python scripts/checking.py
+        echo "Pre-launch checks completed.";;
+    * ) echo "Skipping pre-launch checks.";;
+esac
+
+# Launch app in new terminal
+echo "Launching application..."
+cd "$WORK1"
+python catkin_ws/src/grasp_demo_app/scripts/main.py
+echo "Application has finished."
+
+
+# Run in new terminal
+# roslaunch grasp_demo_app debug_moveit.launch
