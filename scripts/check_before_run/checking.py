c7aa4f3145a8066a62cb41380743039a3eb0c5a1
Xi Chen <407081305@qq.com>
2024-11-12 15:11:10 +0800

    script to check and load

---
+++ scripts/check_before_run/checking.py
@@ -0,0 +1,34 @@
+import os, sys
+module_path = os.path.abspath(os.path.join('.'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+CHECK_CAMERA_CONNECTION = True
+CHECK_CLASSIFIER = True
+
+if CHECK_CAMERA_CONNECTION:
+    from pointcloud_processing.realsense_capture import realsense_instance
+    import numpy as np
+    rgb = realsense_instance.get_rgb_frame()
+    if rgb is not None:
+        print(f'Camera connected, shape: {rgb.shape}')
+        
+        
+    pcd, rgb = realsense_instance.capture()
+    if pcd is not None: 
+        points = np.asarray(pcd.points)
+        print(f'Point cloud captured, shape: {points.shape}') 
+    
+    
+if CHECK_CLASSIFIER:
+    import os, sys
+    module_path = os.path.abspath('catkin_ws/src/grasp_demo_app')
+    if module_path not in sys.path:
+        sys.path.append(module_path)
+
+    from core.object_classifier import get_object_name_from_clip
+    import cv2
+    cv2.imwrite('temp.jpg', rgb)
+    object_name = get_object_name_from_clip('temp.jpg')
+    print(f'Object Classifier Ready, object name: {object_name}')
+    
